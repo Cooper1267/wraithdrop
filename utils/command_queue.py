14f6403f8ebc2f0b06a71231d284c3796e8363fa
MasterAncestor <leeandrew330@gmail.com>
2025-08-13 06:31:24 -0400

    Update command_queue.py

--- utils/command_queue.py
+++ utils/command_queue.py
@@ -1,7 +1,25 @@
-COMMAND_QUEUE = {}
+from collections import defaultdict, deque
+import threading
 
-def get(client_id):
-    if client_id in COMMAND_QUEUE and COMMAND_QUEUE[client_id]:
-        return COMMAND_QUEUE[client_id].pop(0)
-    return None
+# Thread-safe command queue implementation
+class CommandQueue:
+    def __init__(self):
+        self.queues = defaultdict(deque)
+        self.lock = threading.Lock()
 
+    def put(self, client_id, command):
+        with self.lock:
+            self.queues[client_id].append(command)
+
+    def get(self, client_id):
+        with self.lock:
+            if self.queues[client_id]:
+                return self.queues[client_id].popleft()
+            return None
+
+    def is_empty(self, client_id):
+        with self.lock:
+            return not bool(self.queues[client_id])
+
+# Usage
+COMMAND_QUEUE = CommandQueue()
